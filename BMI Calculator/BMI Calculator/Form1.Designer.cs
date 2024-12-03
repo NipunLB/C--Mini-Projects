0d610d454180e30ff4634a8f8aa2f2cebefd9b21
Nipun Lakshan Bandara <nagaharala8484@gmail.com>
2024-12-03 20:37:37 +0530

    Add files via upload

---
+++ BMI Calculator/BMI Calculator/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace BMI_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtBmi = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtIndicator = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblIndicator = new System.Windows.Forms.Label();
+            this.lblBmi = new System.Windows.Forms.Label();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(141, 62);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(165, 22);
+            this.txtHeight.TabIndex = 0;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(141, 133);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(165, 22);
+            this.txtWeight.TabIndex = 1;
+            // 
+            // txtBmi
+            // 
+            this.txtBmi.Location = new System.Drawing.Point(141, 204);
+            this.txtBmi.Name = "txtBmi";
+            this.txtBmi.Size = new System.Drawing.Size(165, 22);
+            this.txtBmi.TabIndex = 2;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(496, 62);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(165, 22);
+            this.txtStatus.TabIndex = 3;
+            // 
+            // txtIndicator
+            // 
+            this.txtIndicator.Location = new System.Drawing.Point(496, 133);
+            this.txtIndicator.Name = "txtIndicator";
+            this.txtIndicator.Size = new System.Drawing.Size(165, 22);
+            this.txtIndicator.TabIndex = 4;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(400, 68);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblIndicator
+            // 
+            this.lblIndicator.AutoSize = true;
+            this.lblIndicator.Location = new System.Drawing.Point(400, 139);
+            this.lblIndicator.Name = "lblIndicator";
+            this.lblIndicator.Size = new System.Drawing.Size(58, 16);
+            this.lblIndicator.TabIndex = 6;
+            this.lblIndicator.Text = "indicator";
+            // 
+            // lblBmi
+            // 
+            this.lblBmi.AutoSize = true;
+            this.lblBmi.Location = new System.Drawing.Point(38, 210);
+            this.lblBmi.Name = "lblBmi";
+            this.lblBmi.Size = new System.Drawing.Size(30, 16);
+            this.lblBmi.TabIndex = 7;
+            this.lblBmi.Text = "BMI";
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(38, 139);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(76, 16);
+            this.lblWeight.TabIndex = 8;
+            this.lblWeight.Text = "Weight (Kg)";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(38, 62);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(68, 16);
+            this.lblHeight.TabIndex = 9;
+            this.lblHeight.Text = "Height (m)";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(496, 290);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(165, 50);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(141, 290);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(165, 50);
+            this.btnCalculate.TabIndex = 11;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.lblBmi);
+            this.Controls.Add(this.lblIndicator);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtIndicator);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtBmi);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtHeight);
+            this.Name = "Form1";
+            this.Text = "Free BMI Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtBmi;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.TextBox txtIndicator;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblIndicator;
+        private System.Windows.Forms.Label lblBmi;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ BMI Calculator/BMI Calculator/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtBmi.Clear();
+            txtHeight.Clear();
+            txtIndicator.Clear();
+            txtIndicator.BackColor = Color.White;
+            txtStatus.Clear();
+            txtWeight.Clear();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double heightX;
+            heightX = Convert.ToDouble(txtHeight.Text);
+            double weightY;
+            weightY = Convert.ToDouble(txtWeight.Text);
+
+            double bmiAnswer;
+            bmiAnswer = weightY / Math.Pow(heightX,2);
+
+            //Display the ansewer
+            txtBmi.Text = bmiAnswer.ToString();
+
+            //Display the Status
+            if (bmiAnswer < 18.5)
+            {
+                txtStatus.Text = "Under Weight";
+                txtIndicator.BackColor = Color.Red;
+            }
+            else {
+                if (bmiAnswer < 25)
+                {
+                    txtStatus.Text = "Normal";
+                    txtIndicator.BackColor = Color.Blue;
+                }
+                else {
+                    if (bmiAnswer < 30)
+                    {
+                        txtStatus.Text = "Obesity Class I";
+                        txtIndicator.BackColor = Color.Orange;
+                    }
+                    else {
+                        if (bmiAnswer < 40)
+                        {
+                            txtStatus.Text = "Obesity Class II";
+                            txtIndicator.BackColor = Color.Pink;
+                        }
+                        else {
+                            if (40 < bmiAnswer) {
+                                txtStatus.Text = "Obesity Class III";
+                                txtIndicator.BackColor = Color.Brown;
+                            }
+                        }
+                    }
+                }
+            }
+           
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
